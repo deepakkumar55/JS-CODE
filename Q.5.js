4cff98676882fd5c3bd88e9fcf5ca8e9c7d722bf
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-06-10 11:55:56 +0530

    .

--- Q.5.js
+++ Q.5.js
@@ -1,8 +1,8 @@
-// 5. Create a function that reverses an array
+// 5. Create a function that reverses an array; 
 
 arr=[1,2,3,42,4,5,3,5,6,10];
 var arr2=[];
 for ( i = arr.length - 1; i >= 0; i--) {
     arr2.push(arr[i]);
 }
-console.log(arr2);
+console.log(arr2); 
